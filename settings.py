f979ee253efc61bf19d8f146023b8f4a6ec7dbb2
Artyom <rektju@gmail.com>
2024-02-10 08:49:08 +0300

    source files moved

--- settings.py
+++
@@ -1,7 +0,0 @@
-def load_settings():
-    settings = {}
-    with open('settings.txt') as f:
-        for line in f:
-            (key, val) = line.split('=')
-            settings[key] = val
-    return settings
